4fdbbf4fc9fe0b5d436f68096e8b42faaacec4df
kauany-marques <kauany.silva@sptech.school>
2025-11-27 00:36:11 -0300

    subindo Chamados

---
+++ public/js/modal.js
@@ -0,0 +1,7 @@
+function abrirModal() {
+  document.getElementById('modalChamado').style.display = 'flex';
+}
+
+function fecharModal() {
+  document.getElementById('modalChamado').style.display = 'none';
+}
