22169338b8f9e8c1cce34d34827dfdd101127f3a
flavioroberti <flavio.roque@sptech.school>
2025-12-03 20:00:00 -0300

    Model dos Chamados

--- src/models/chamadoModel.js
+++ src/models/chamadoModel.js
@@ -1,81 +1,55 @@
-var database = require("../database/config")
+var database = require("../database/config");
 
-function enviarChamado(id, assunto, descricao) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function enviarChamado():", id, assunto, descricao);
+function enviarChamado(id, assunto, descricao, tipo) {
+    console.log("ACESSEI O CHAMADO MODEL - enviarChamado:", id, assunto, descricao, tipo);
 
-    var instrucaoSql = `
-            INSERT INTO TB_Chamados (id_usuario, assunto, descricao) VALUES ('${id}', '${assunto}', '${descricao}');
-        `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+    var instrucaoSql = `CALL SP_EnviarChamado(?, ?, ?, ?);`;
+    var parametros = [id, assunto, descricao, tipo];
+
+    return database.executarComParametros(instrucaoSql, parametros)
+        .then(resultado => {
+            console.log("Resultado da PROC:", resultado[0][0]);
+            return resultado[0][0];
+        });
 }
 
 function carregarChamados(id, perfil) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function carregarChamados():", id, perfil);
+    console.log("ACESSEI O CHAMADO MODEL - carregarChamados:", id, perfil);
 
-    if (perfil == 1) {
-        var instrucaoSql = `
-            SELECT 
-                c.id AS id_chamado,
-                u.nome AS nome,
-                u.email AS email,
-                p.cargo AS perfil,
-                e.razao_social,
-                c.assunto,
-                c.descricao,
-                c.status,
-                DATE_FORMAT(c.data_chamado, '%d/%m/%Y') AS data_chamado
-            FROM TB_Chamados c
-            JOIN TB_Usuarios u ON c.id_usuario = u.id
-            JOIN TB_Perfis p ON u.id_perfil = p.id
-            LEFT JOIN TB_Empresas e ON u.id_empresa = e.id
-            WHERE u.id = ${id}
-              AND c.status != 'Descontinuado';
-        `;
-        console.log("Executando a instrução SQL: \n" + instrucaoSql);
-        return database.executar(instrucaoSql);
-    } else {
-        var instrucaoSql = `
-            SELECT 
-                c.id AS id_chamado,
-                u.nome AS nome,
-                u.email AS email,
-                p.cargo AS perfil,
-                e.razao_social,
-                c.assunto,
-                c.descricao,
-                c.status,
-                DATE_FORMAT(c.data_chamado, '%d/%m/%Y') AS data_chamado
-            FROM TB_Chamados c
-            JOIN TB_Usuarios u ON c.id_usuario = u.id
-            JOIN TB_Perfis p ON u.id_perfil = p.id
-            LEFT JOIN TB_Empresas e ON u.id_empresa = e.id;
-        `;
-        console.log("Executando a instrução SQL: \n" + instrucaoSql);
-        return database.executar(instrucaoSql);
-    }
+    var instrucaoSql = `CALL SP_CarregarChamados(?, ?);`;
+    var parametros = [id, perfil];
+
+    return database.executarComParametros(instrucaoSql, parametros)
+        .then(resultado => {
+            console.log("Resultado da PROC:", resultado[0]);
+            return resultado[0];
+        });
 }
 
 function finalizarChamado(idChamado) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function finalizarChamado():", idChamado);
+    console.log("ACESSEI O CHAMADO MODEL - finalizarChamado:", idChamado);
 
-    var instrucaoSql = `
-        UPDATE TB_Chamados SET status = 'Finalizado' WHERE id = ${idChamado} AND status = 'Aberto';
-    `;
+    var instrucaoSql = `CALL SP_FinalizarChamado(?);`;
+    var parametros = [idChamado];
 
-    console.log("Executando SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+    return database.executarComParametros(instrucaoSql, parametros)
+        .then(resultado => {
+            console.log("Resultado da PROC:", resultado[0][0]);
+            return resultado[0][0];
+        });
 }
 
 function deletarChamado(idChamado) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function deletarChamado():", idChamado);
+    console.log("ACESSEI O CHAMADO MODEL - deletarChamado:", idChamado);
 
-    var instrucaoSql = `
-        UPDATE TB_Chamados SET status = 'Descontinuado' WHERE id = ${idChamado} AND status = 'Aberto';
-    `;
+    var instrucaoSql = `CALL SP_DeletarChamado(?);`;
+    var parametros = [idChamado];
 
-    console.log("Executando SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+    return database.executarComParametros(instrucaoSql, parametros)
+        .then(resultado => {
+            console.log("Resultado da PROC:", resultado[0][0]);
+            return resultado[0][0];
+        });
 }
 
 module.exports = {
